a535b8e1d32087985308fd2d19fbd4de5b043956
Sergii Podriezienko <podreenko1986@gmail.com>
2023-02-17 12:23:54 +0200

    second changes
    second changes

--- src/partials/weather.js
+++ src/partials/weather.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', loadWeatherDate);
+
+function loadWeatherDate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSucces, OnError);
+  } else {
+    alert('Your browser does not support geolocation');
+  }
+}
+
+function onSucces(position) {
+  const { latitude, longitude } = position.coords;
+  console.log(latitude, longitude);
+}
+
+function OnError() {}
+
 export * as weather from './weather.js';
