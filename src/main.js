e9282d5be88c7db9cb5879b08de72ce3223c549f
DenMerch <rosignolli@gmail.com>
2023-02-18 00:00:26 +0200

    add conteiner

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { calendarTools } from './partials/calendar';
 import { apiFetchNewsByValue } from './partials/apiFetchNewsByValue';
 import { fetchBaseNews } from "./partials/api-handler";
+import { } from "./partials/categories.js"
 
