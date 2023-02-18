0ea0020d2e09262514fd417ce331b74ffd6e5c75
Yevhenii Strelbitskyi <strelbitskii.evgenii@gmail.com>
2023-02-18 17:50:26 +0200

    first commit

--- src/partials/api-categories.js
+++ src/partials/api-categories.js
@@ -1,10 +1,11 @@
-const API_KEY = 'api-key=Mw0nOoO0CyWfJRrshsqkL1haZT52Fizf';
+import NewsApi from './apiConstructor'
+// const API_KEY = 'api-key=Mw0nOoO0CyWfJRrshsqkL1haZT52Fizf';
 // const API_KEY = "api-key=9GYTd3hNgT1cJMME7q1HMJAu02NGsmfm";
 // const API_KEY = 'api-key=JmGuT2FnDagHatExdMuVy4QCYQRUlSyR';
 
-const API_HOST = 'https://api.nytimes.com';
-const WEB_HOST = 'https://www.nytimes.com';
-const CATEGOTY_END_POINT = `${API_HOST}/svc/news/v3/content/all/`
+// const API_HOST = 'https://api.nytimes.com';
+// const WEB_HOST = 'https://www.nytimes.com';
+// const CATEGOTY_END_POINT = `${API_HOST}/svc/news/v3/content/all/`
 
 
 const categoryList = document.querySelector('.category');
@@ -23,7 +24,7 @@ function categorySelect (event) {
 
 async function searchByCategory (handledCategory) {
    try {
-    const response = await fetch(`${CATEGOTY_END_POINT}${handledCategory}.json?${API_KEY}`);
+    const response = await fetch(`${NewsApi.ACATEGOTY_END_POINT}${handledCategory}.json?${NewsApi.API_KEY}`);
     // console.log(response)
     const articles = await response.json();
     // console.log(articles)
