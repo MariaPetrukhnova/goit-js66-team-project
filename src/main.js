9b817fd6a68a3a2bea282d50301ba9f6e3b84a3d
EugeneKom <EugeneKomissarenko@gmail.com>
2023-02-18 18:14:07 +0100

    add slice
    добавил функцию для обрезки текста

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@
 import { calendarTools } from './partials/calendar';
 import { fetchPopularNews } from './partials/try_api';
 import { apiFetchNewsByValue } from './partials/apiFetchNewsByValue';
-import { } from './partials/categories.js';
+import {} from './partials/categories.js';
 import * as articles from './partials/articles';
