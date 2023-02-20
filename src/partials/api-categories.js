1963697ec10e914e59ead7901edd42f16053e132
Yevhenii Strelbitskyi <strelbitskii.evgenii@gmail.com>
2023-02-20 19:10:58 +0200

    maybe end

--- src/partials/api-categories.js
+++ src/partials/api-categories.js
@@ -1,14 +1,8 @@
-import NewsApi from './apiConstructor.js';
+import NewsApi from './apiConstructor.js'
 import { makeMarkup, addMarkup } from './renderMarkup';
-const API_KEY = 'api-key=Mw0nOoO0CyWfJRrshsqkL1haZT52Fizf';
-// const API_KEY = "api-key=9GYTd3hNgT1cJMME7q1HMJAu02NGsmfm";
-// const API_KEY = 'api-key=JmGuT2FnDagHatExdMuVy4QCYQRUlSyR';
+const newsApi = new NewsApi();
 
-
-
-const newsApi = new NewsApi()
 const categoryList = document.querySelector('.category__list');
-
 
 categoryList.addEventListener('click', categorySelect)
 
@@ -24,47 +18,41 @@ function categorySelect (event) {
 
 async function searchByCategory (handledCategory) {
    try {
-    const response = await fetch(`${newsApi.CATEGOTY_END_POINT}${handledCategory}.json?${API_KEY}`);
-    const articles = await response.json();
+    const response = await fetch(`${newsApi.CATEGOTY_END_POINT}${handledCategory}.json?${newsApi.API_KEY}`);
+    const articles = await response.json()
     const resArr = articles.results;
-    // const answer = arrHandler(resArr);
-    console.log(resArr)
+    arrHandler(resArr);
+    
    }
    catch (error) {
     console.log(error);
   }   
 };
 
-function arrHandler (arr){
-  const respObj = arr.map(item => {
+function arrHandler(arr) {
+  const objArr = arr.map(el => {
+    if (el.multimedia === null) {
+      return {
+        section: el.section_name || el.section,
+        title: el.title || el.headline.main,
+        description: el.abstract,
+        url: el.web_url || el.url,
+        date: el.pub_date || el.created_date,
+        imgCaption: el.title,
+        img: `https://cdn.pixabay.com/photo/2013/03/30/00/10/news-97862_960_720.png`,
+      };
+    }
     return {
-      title: item.title || item.abstract,
-      description: item.abstract,
-      url: item.url,
-      date: item.published_date || item.created_date,
-      img: `${item.multimedia[0].url}`,
-      imgCaption: `${item.multimedia[0].caption}`
+      section: el.section_name || el.section,
+      title: el.title || el.headline.main,
+      description: el.abstract,
+      url: el.web_url || el.url,
+      date: el.pub_date || el.created_date,
+      imgCaption: el.multimedia[2].caption,
+      img: el.multimedia[2].url,
     };
-
   });
-  return respObj
-//  const markup = createCards(respObj);
-//  console.log(markup);
+  const finalMarkup = makeMarkup(objArr);
+  addMarkup(finalMarkup);
 }
-
-
-// function createCards (respObj) {
-//   const markup = respObj.reduce((string, item) => createCard(item)+string,"")
-//   return markup
-// }
-
-// function createCard({title, description, url, date, img}) {
-//   return `
-//   <div class="card">
-//         <img src=${img} alt="${imgCaption}">
-//         <h3>${title}</h3>
-//         <p>${description}</p>
-//         <a href="${url}"></a>
-//     </div>`
-// }
 export * as apiCategoties from './api-categories.js';
