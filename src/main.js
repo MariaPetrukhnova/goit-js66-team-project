4d8ddd253df4bb244b74d40a13bb9aca076c9716
IHOR <lavrenchyk.igor@gmail.com>
2023-02-20 19:01:30 +0200

    3
    додав import

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { refsEl } from '../src/partials/refs.js'
+import { refsEl } from '../src/partials/refs.js';
 import * as myModule from './partials/mobile-version';
 import { calendarTools } from './partials/calendar';
 import { loadWeatherDate } from './partials/weather';
 import { fetchPopularNews } from './partials/try_api';
 import { apiFetchNewsByValue } from './partials/apiFetchNewsByValue';
-import { } from './partials/categories.js';
+import {} from './partials/categories.js';
 import * as articles from './partials/articles';
+import {} from './partials/paginations';
