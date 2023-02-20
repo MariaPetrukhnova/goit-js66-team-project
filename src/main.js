39c72c745728c85cff3f5b0aa3f4d4a937b4fc6b
Sergii Podriezienko <podreenko1986@gmail.com>
2023-02-20 08:38:55 +0200

    correct import loadWeatherDate
    correct import loadWeatherDate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //<<<<<<< HEAD
 //import { apiTools } from './partials/api-handler';
 import { calendarTools } from './partials/calendar';
-import { weather } from './partials/weather';
+import { loadWeatherDate } from './partials/weather';
 //=======
 import { fetchNewsBySearch, fetchBaseNews } from './partials/api-handler';
 
