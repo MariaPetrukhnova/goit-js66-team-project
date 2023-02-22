c25dcfdf537cbcf10ed6f82470c78df050e83ddd
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-02-22 13:59:22 +0000

    Deploying to gh-pages from @ MariaPetrukhnova/goit-js66-team-project@3cf8ceb53bf4daef268592da834c5d3ae76d3f7a 🚀

---
+++ assets/markup-eca31c3f.js
@@ -0,0 +1,27 @@
+import{s as r}from"./localeStoreageHandler-ff8a1066.js";function g(a,i=!0){return(i?m(a):a).map(e=>{if(!e)return;const{section:s,title:d,description:c,url:n,date:o,img:l,imgCaption:p,id:u}=e;return`<li class="article" data-id="${u}">
+     <div class="article_img_wrapper">
+       <p class="already-read is-hidden">Already read</p>
+       <p class="article_category">${s}</p>
+       <img class="article_img" src="${l}" alt="${p}" width="395" height="395">
+       <div class="article_flag">
+       <button class="favorites-button article_flag--add"><span class="article_flag_text">Add to favorite</span>
+         <svg width="16" height="16">
+         <use href="${r}#heart_contur" width="16" height="16"></use>
+        </svg>
+         </button>
+         <button class="favorites-button article_flag--remove is-hidden"><span class="article_flag_text">Remove from favorite</span>
+         <svg width="16" height="16">
+         <use href="${r}#heart_fill" width="16" height="16"></use>
+       </svg>
+          </button>
+         </div>
+     </div>
+     <div class="article_text_wrapper">
+       <h2 class="article_title">${d}</h2>
+       <p class="article_text">${c}</p>
+     </div>
+     <div class="article_info_wrapper">
+       <p class="article_date">${o}</p>
+       <a href="${n}" class="read-more" target="_blank">Read more</a>
+     </div>
+     </li>`}).join("")}function m(a){try{return a.map(t=>t.media.length===0?{section:t.section_name||t.section,title:t.title||t.headline.main,description:t.abstract,url:t.web_url||t.url,date:t.pub_date||t.created_date||t.published_date,img:"https://cdn.pixabay.com/photo/2013/03/30/00/10/news-97862_960_720.png",imgCaption:"image",id:t.id}:{section:t.section_name||t.section,title:t.title||t.headline.main,description:t.abstract,url:t.web_url||t.url,date:t.pub_date||t.created_date||t.published_date,img:t.media[0]["media-metadata"][2].url,imgCaption:t.media[0].caption,id:t.id})}catch(i){console.error(i)}}export{g as c};
