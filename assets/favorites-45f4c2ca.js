9d2419e1d8c5274bba44223d8803fb06e51198a1
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-02-22 12:49:00 +0000

    Deploying to gh-pages from @ MariaPetrukhnova/goit-js66-team-project@f57ba6f7a9771d219273ad2faac5ba85981a27a7 🚀

--- assets/favorites-45f4c2ca.js
+++ assets/favorites-45f4c2ca.js
@@ -1,8 +1,8 @@
-import{s as a}from"./mobile-version-de24c2cd.js";const g="favorite-articles";document.addEventListener("DOMContentLoaded",e=>{e.preventDefault(),document.querySelector(".news-gallery").innerHTML=m()});function m(){const e=JSON.parse(localStorage.getItem(g));return!e||e.length===0?"<h2 class='articles-not-found'>You don't have favorite articles yet</h2><img class='not-found-img' src='./images/not-found-desktop-1x.png' alt='no articles there'>":e.map(t=>{if(!t)return;const{section:n,title:c,description:o,url:d,date:h,img:p,imgCaption:u,id:_}=t;return`<li class="article" data-id="${_}">
+import{s as a}from"./mobile-version-de24c2cd.js";const g="favorite-articles";document.addEventListener("DOMContentLoaded",e=>{e.preventDefault(),document.querySelector(".articles_container").innerHTML=f()});function f(){const e=JSON.parse(localStorage.getItem(g));return!e||e.length===0?"<li class='not-found-container'><h2 class='articles-not-found'>You don't have favorite articles yet</h2><img class='not-found-img' src='./images/not-found-desktop-1x.png' alt='no articles there'></li>":e.map(t=>{if(!t)return;const{section:n,title:c,description:o,url:d,date:u,img:h,imgCaption:p,id:_}=t;return`<li class="article" data-id="${_}">
      <div class="article_img_wrapper">
        <p class="already-read is-hidden">Already read</p>
        <p class="article_category">${n}</p>
-       <img class="article_img" src="${p}" alt="${u}" width="395" height="395">
+       <img class="article_img" src="${h}" alt="${p}" width="395" height="395">
        <div class="article_flag">
        <button class="article_flag--add  is-hidden"><span class="article_flag_text">Add to favorite</span>
          <svg width="16" height="16">
@@ -21,7 +21,7 @@ import{s as a}from"./mobile-version-de24c2cd.js";const g="favorite-articles";doc
        <p class="article_text">${o}</p>
      </div>
      <div class="article_info_wrapper">
-       <p class="article_date">${h}</p>
+       <p class="article_date">${u}</p>
        <a href="${d}" class="read-more" target="_blank">Read more</a>
      </div>
      </li>`}).join("")}var r,i,s,l;window.location.pathname==="/"?(i=(r=document.querySelector(".navbar__link"))==null?void 0:r.classList)==null||i.add("navbar__link--current"):(l=(s=document.querySelector(`.navbar__link[href=".${window.location.pathname}"]`))==null?void 0:s.classList)==null||l.add("navbar__link--current");
